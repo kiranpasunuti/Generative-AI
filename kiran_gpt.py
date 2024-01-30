373e28a1acd59ff05e38c21bcebe79abb8e2ec5f
saikiran <126646670+kiranpasunuti@users.noreply.github.com>
2024-01-30 08:12:24 +0530

    Update kiran_gpt.py

--- kiran_gpt.py
+++ kiran_gpt.py
@@ -7,7 +7,6 @@ from langchain.llms import GooglePalm
 def generate_random_color():
     return "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-# Set background color and page style
 st.markdown(
     f"""
     <style>
@@ -25,25 +24,15 @@ st.markdown("<h1 style='text-align: center; color: #ff6600;'><strong>Kiran GPT</
 # Initialize GooglePalm model
 api_key = "AIzaSyA4ACANQRcchCNDXamY0Neyp9Yo_2dvK8w"
 google_palm = GooglePalm(google_api_key=api_key, temperature=0.7)
-
-# Function to generate predictions
 def generate_prediction(prompt):
     # Interaction with GooglePalm
     response = google_palm.predict(prompt)
     return response
-
-# Streamlit App
 def main():
-    # User input
     prompt = st.text_area("Enter your prompt:")
-
-    # Generate button with animation
     if st.button("Generate Response", key="generate_button"):
         if prompt:
-            # Generate response using GooglePalm model
             response = generate_prediction(prompt)
-            
-            # Display response with colorful text
             st.markdown(f"<p style='color: {generate_random_color()}; font-size: 18px;'>Generated Response:</p>", unsafe_allow_html=True)
             st.write(response)
         else:
